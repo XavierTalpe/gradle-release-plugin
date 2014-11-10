1865e6d83f68cc68a9ab278321162a2b292c877f
Xavier Talpe <xaviertalpe+pro@gmail.com>
2014-11-10 22:45:22 +0100

    Extracted some additional integration tests.

--- gradle-release-plugin/src/test/groovy/be/xvrt/gradle/release/plugin/UpdateVersionTaskTest.groovy
+++ gradle-release-plugin/src/test/groovy/be/xvrt/gradle/release/plugin/UpdateVersionTaskTest.groovy
@@ -9,7 +9,6 @@ import org.junit.Test
 import org.junit.rules.TemporaryFolder
 
 import static org.junit.Assert.assertEquals
-import static org.junit.Assert.assertTrue
 
 class UpdateVersionTaskTest {
 
@@ -57,53 +56,6 @@ class UpdateVersionTaskTest {
         assertEquals( '1.0.0', updateVersionTask.releasedVersion )
         assertEquals( '1.0.1', updateVersionTask.nextVersion )
         assertEquals( '1.0.1', project.version )
-    }
-
-    @Test
-    void testNextReleaseWithEmptyPropertiesFile() {
-        setup:
-        def propertiesFile = temporaryFolder.newFile( 'gradle.properties' )
-
-        def project = ProjectBuilder.builder().withProjectDir( temporaryFolder.root ).build()
-        project.apply plugin: ReleasePlugin
-        project.version = '1.0.0-SNAPSHOT'
-
-        def prepareReleaseTask = project.tasks.getByName ReleasePlugin.PREPARE_RELEASE_TASK
-        def updateVersionTask = project.tasks.getByName ReleasePlugin.UPDATE_VERSION_TASK
-
-        when:
-        prepareReleaseTask.configure()
-        updateVersionTask.execute()
-
-        then:
-        def properties = new Properties()
-        propertiesFile.withInputStream { properties.load( it ) }
-
-        assertTrue( properties.isEmpty() )
-    }
-
-    @Test
-    void testNextReleaseWithPropertiesFile() {
-        setup:
-        def propertiesFile = temporaryFolder.newFile( 'gradle.properties' )
-        propertiesFile << 'version=1.0.0-SNAPSHOT'
-
-        def project = ProjectBuilder.builder().withProjectDir( temporaryFolder.root ).build()
-        project.apply plugin: ReleasePlugin
-        project.version = '1.0.0-SNAPSHOT' // TODO: Trigger project to read properties file instead.
-
-        def prepareReleaseTask = project.tasks.getByName ReleasePlugin.PREPARE_RELEASE_TASK
-        def updateVersionTask = project.tasks.getByName ReleasePlugin.UPDATE_VERSION_TASK
-
-        when:
-        prepareReleaseTask.configure()
-        updateVersionTask.execute()
-
-        then:
-        def properties = new Properties()
-        propertiesFile.withInputStream { properties.load( it ) }
-
-        assertEquals( '1.0.1-SNAPSHOT', properties.version )
     }
 
     @Test
