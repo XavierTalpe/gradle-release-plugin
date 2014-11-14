fe58ebfd5e3b996e25a627bb3d830234659e9ffd
Xavier Talpe <xavier.talpe@tomtom.com>
2014-11-14 17:38:51 +0100

    Utility method to add origin to git repo.

--- gradle-release-plugin/src/test/groovy/be/xvrt/gradle/release/plugin/scm/ScmTestUtil.groovy
+++ gradle-release-plugin/src/test/groovy/be/xvrt/gradle/release/plugin/scm/ScmTestUtil.groovy
@@ -1,11 +1,8 @@
 package be.xvrt.gradle.release.plugin.scm
 
 import org.eclipse.jgit.api.Git
-import org.eclipse.jgit.lib.Config
 import org.eclipse.jgit.lib.Repository
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder
-import org.eclipse.jgit.transport.RemoteConfig
-import org.eclipse.jgit.transport.URIish
 
 class ScmTestUtil {
 
@@ -23,17 +20,11 @@ class ScmTestUtil {
         repo
     }
 
-    // TODO
-    static Repository addOrigin( Repository repository, Repository originRepository ) {
-        def config = new Config()
-        config.setString( "remote", "origin", "pushurl", "short:project.git" )
-        config.setString( "url", "https://server/repos/", "name", "short:" )
-
-        RemoteConfig remoteConfig = new RemoteConfig( new Config(), "origin" )
-        remoteConfig.addURI( new URIish( originRepository.getDirectory().toURI().toURL() ) )
-
-        def git = new Git( repository )
-        git.push().setRemote( originRepository.getDirectory().toURI().toString() ).call()
+    static void addOrigin( Repository repository, Repository originRepository ) {
+        def configFile = new File( repository.getDirectory(), 'config' )
+        configFile << '[remote "origin"]'
+        configFile << "\n\turl = file://${originRepository.getDirectory()}"
+        configFile << "\n\tfetch = +refs/heads/*:refs/remotes/origin/*"
     }
 
 }
