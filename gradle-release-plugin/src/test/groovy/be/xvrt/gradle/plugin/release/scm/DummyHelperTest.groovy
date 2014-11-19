ff30702d62efdbe3aa443f76480acfd819d2bd3f
Xavier Talpe <xavier.talpe@tomtom.com>
2014-11-19 16:25:34 +0100

    Added dummy test.

---
+++ gradle-release-plugin/src/test/groovy/be/xvrt/gradle/plugin/release/scm/DummyHelperTest.groovy
@@ -0,0 +1,25 @@
+package be.xvrt.gradle.plugin.release.scm
+
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+
+class DummyHelperTest {
+
+    @Rule
+    public final TemporaryFolder temporaryFolder = new TemporaryFolder()
+
+    @Test
+    void 'dummy helper does nothing'() {
+        def dummyHelper = ScmHelperFactory.create( temporaryFolder.root )
+
+        def commitId = dummyHelper.commit 'commitMessage'
+        dummyHelper.deleteCommit commitId
+
+        def tagId = dummyHelper.tag '1.0.0', 'tagMessage'
+        dummyHelper.deleteTag tagId
+
+        dummyHelper.push 'origin'
+    }
+
+}
